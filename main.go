d3d95379b6be28f201c0cf0388f974d56a9c7d07
Christopher Palmer <chrisjpalmer@optusnet.com.au>
2019-02-02 10:38:26 +1100

    added code which connects the grpc server to the word_search_service

--- main.go
+++ main.go
@@ -13,35 +13,55 @@ const (
 	port = ":50051"
 )
 
-type server struct{}
-
-func (s *server) SearchWord(ctx context.Context, in *wordsearchsystemgrpc.SearchWordRequest) (*wordsearchsystemgrpc.SearchWordReply, error) {
-	return nil, nil
+type wordSearchSystemServer struct {
+	wordSearchService *WordSearchService
 }
 
-func (s *server) AddWords(ctx context.Context, in *wordsearchsystemgrpc.AddWordsRequest) (*wordsearchsystemgrpc.AddWordsReply, error) {
-	return nil, nil
+func newWordSearchSystemServer(wordSearchService *WordSearchService) *wordSearchSystemServer {
+	_newWordSearchSystemServer := new(wordSearchSystemServer)
+	_newWordSearchSystemServer.wordSearchService = wordSearchService
+	return _newWordSearchSystemServer
 }
 
-func (s *server) Top5SearchKeyWords(ctx context.Context, in *wordsearchsystemgrpc.Top5SearchKeyWordsRequest) (*wordsearchsystemgrpc.Top5SearchKeyWordsReply, error) {
-	return nil, nil
+func (_wordSearchSystemServer *wordSearchSystemServer) SearchWord(ctx context.Context, in *wordsearchsystemgrpc.SearchWordRequest) (*wordsearchsystemgrpc.SearchWordReply, error) {
+	matches := _wordSearchSystemServer.wordSearchService.SearchWord(in.KeyWord)
+	return &wordsearchsystemgrpc.SearchWordReply{Matches: matches}, nil
+}
+
+func (_wordSearchSystemServer *wordSearchSystemServer) AddWords(ctx context.Context, in *wordsearchsystemgrpc.AddWordsRequest) (*wordsearchsystemgrpc.AddWordsReply, error) {
+	err := _wordSearchSystemServer.wordSearchService.AddWords(in.Words)
+	if err != nil {
+		return nil, err
+	}
+	return &wordsearchsystemgrpc.AddWordsReply{}, nil
+}
+
+func (_wordSearchSystemServer *wordSearchSystemServer) Top5SearchKeyWords(ctx context.Context, in *wordsearchsystemgrpc.Top5SearchKeyWordsRequest) (*wordsearchsystemgrpc.Top5SearchKeyWordsReply, error) {
+	keyWords := _wordSearchSystemServer.wordSearchService.Top5SearchKeyWords()
+	return &wordsearchsystemgrpc.Top5SearchKeyWordsReply{Keywords: keyWords}, nil
 }
 
 func main() {
-	//Create new socket connection to listen on the port
-	socketConnection, err := net.Listen("tcp", port)
+	//Create the word search service
+	wordSearchService := NewWordSearchService()
+
+	//Create the listener for the specific port
+	listener, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	//Instantiate new grpc server instance class
+	//Instantiate new grpc server instance object
 	grpcServer := grpc.NewServer()
 
-	//Register the WordSearchSystem proto with the grpc server instance
-	wordsearchsystemgrpc.RegisterWordSearchSystemServer(grpcServer, &server{})
+	//Create the WordSearchSystemServer
+	wordSearchSystemServer := newWordSearchSystemServer(wordSearchService)
 
-	//Call server on the grpc server passing the socket connection
-	if err := grpcServer.Serve(socketConnection); err != nil {
+	//Connect the Server, with the proto definitions with the instance of the grpcServer
+	wordsearchsystemgrpc.RegisterWordSearchSystemServer(grpcServer, wordSearchSystemServer)
+
+	//Connect the grpcServer with the listener so that it can begin accepting requests
+	if err := grpcServer.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
--- word_search_service.go
+++ word_search_service.go
@@ -133,7 +133,7 @@ func (wordSearchService *WordSearchService) wordsToLowercase(words []string) (lo
 }
 
 //Top5SearchKeyWords - returns the top 5 most searched keywords
-func (wordSearchService *WordSearchService) Top5SearchKeyWords() (keywords []string) {
+func (wordSearchService *WordSearchService) Top5SearchKeyWords() (keyWords []string) {
 	//Clone the source slice
 	keyWordStats := append(wordSearchService.keyWordStats[:0:0], wordSearchService.keyWordStats...)
 
